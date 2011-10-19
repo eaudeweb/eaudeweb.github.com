7edbcde0025e47f4f0e96a2fe0b7035816754f17
dtailed <tudor@dtailed.com>
2011-10-19 23:39:39 +0000

    masonry edits

--- js/edw.js
+++ js/edw.js
@@ -118,7 +118,7 @@ resize_magic = function() {
     if($(".masonry").length)
     {
         $("#we-work-for-container").masonry( 'option', { isAnimated: false });
-        ("#we-work-for-container").masonry('reload');
+        $("#we-work-for-container").masonry('reload');
     }
 	
 	// resize embeds' height
